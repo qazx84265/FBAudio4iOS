fff29e7bff9312e04af8202a30913f7684d6831e
fengbo <fengbo@linewin.cc>
2016-02-22 16:25:08 +0800

    player

--- FBAudio/FBAudioPlayer.m
+++ FBAudio/FBAudioPlayer.m
@@ -7,7 +7,66 @@
 //
 
 #import "FBAudioPlayer.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+
+#define kAudioBufferDurationInSeconds 1
+#define kAudioQueuePreferredBufferNumbers 3
+
+typedef struct {
+    UInt64 mPlayPackets;
+    AudioFileID mPlayAudioFileID;
+    AudioQueueRef mPlayAudioQueue;
+    AudioQueueBufferRef mPlayBuffers[kAudioQueuePreferredBufferNumbers];
+    AudioStreamBasicDescription mPlayASBD;
+    AudioQueueLevelMeterState *mPlayLevels;
+    BOOL mIsPlaying;
+}PlayState;
+
+
+
+@interface FBAudioPlayer() {
+
+}
+@end
+
+
+
 
 @implementation FBAudioPlayer
+
+
+#pragma mark -- init
+
+- (instancetype)init {
+    if (self = [super init]) {
+        
+    }
+    
+    
+    return self;
+}
+
+
+- (void)dealloc {
+
+}
+
+
+
+#pragma mark -- player actions
+
+- (void)play {
+
+}
+
+
+- (void)pause {
+
+}
+
+- (void)stop {
+
+}
 
 @end
